3e8bcfd62791f7eea945bd0ba6872faa6acb5c91
Sasi <sasi.p@vvdntech.in>
2018-09-19 11:45:08 +0530

    set

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1cngo**+ce+6*t^)j*l#rjcs929-fwcw)^go*-m30-l@)ei9$t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
